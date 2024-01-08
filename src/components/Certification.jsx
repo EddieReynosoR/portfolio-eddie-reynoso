34fd888aa2438e7285fc2112b9269b0cc5a425b7
YayoReynoso <eduardo_3reynoso@hotmail.com>
2024-01-07 22:15:20 -0800

    Adding Styles to Portfolio

---
+++ src/components/Certification.jsx
@@ -0,0 +1,16 @@
+export const Certification = () => {
+    return (
+        <article>
+            <div>
+                <div>
+                    <header>
+
+                    </header>
+                    <div>
+                        <h3>Full-Stack Developer</h3>
+                    </div>
+                </div>
+            </div>
+        </article>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import styles from './Navbar.module.css'
 export const NavBar = () => {
     return (
         <nav className={styles.navBar}>
-            <p className={styles.navBarP}><a className={styles.navBarPAnchor} href="#" title='Home'>Home</a></p>
-            <p className={styles.navBarP}><a className={styles.navBarPAnchor} href="#" title='AboutMe'>About me</a></p>
-            <p className={styles.navBarP}><a className={styles.navBarPAnchor} href="#" title='Projects'>Projects</a></p>
+            <p className={styles.navBarP}><a className={styles.navBarPAnchor} href="#about" title='AboutMe'>About me</a></p>
+            <p className={styles.navBarP}><a className={styles.navBarPAnchor} href="#experience" title='Home'>Experience</a></p>
+            <p className={styles.navBarP}><a className={styles.navBarPAnchor} href="#projects" title='Projects'>Projects</a></p>
         </nav>
     )
 }
