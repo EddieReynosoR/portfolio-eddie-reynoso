7edd11c0b9208d8e67b90085e039161700cfd8dc
YayoReynoso <eduardo_3reynoso@hotmail.com>
2024-01-09 21:04:36 -0800

    HomePage changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,18 @@ export const Home = () => {
     return (
         <section className='homeContainer'>
             <header className='homeTitle'>
-                <h1 className='h1Name'>Hi, I'm Eduardo Reynoso</h1>
+                <h1 className='h1Name'>Hey there! 👋 I am...</h1>
                 <div className='developerContainer'>
-                    <h1 className='h1Full'>FULL-STACK</h1>
-                    <h1 className='h1Developer'>DEVELOPER</h1>
+                    <h1 className='h1Full'>EDDIE REYNOSO</h1>
+                    <h1 className='h1Developer'>FULL-STACK DEVELOPER</h1>
                 </div>
-                <div className='buttons'>
-                    <p>I am a developer passionated about the idea of creating useful applications for the persons.</p>
-                    <div className='homeButtons'>
-                    <a href="#" className='hireMeButton' title='HireMe'><span>Hire me</span></a>
-                    <button className='resumeButton'>Resume</button>
+                <div className='buttons-Container'>
+                    <div className='buttons'>
+                        <p>Developer passionated about the idea of creating useful applications for the persons.</p>
+                        <div className='homeButtons'>
+                        <a href="#" className='hireMeButton' title='HireMe'><span>Hire me</span></a>
+                        <button className='resumeButton'>Resume</button>
+                    </div>
             </div>
                 </div>
             </header>
