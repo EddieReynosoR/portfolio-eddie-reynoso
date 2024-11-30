430f028ddd5fc1a7e9852042bd8c1a6b9150de68
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2024-11-29 22:50:02 -0800

    Configuring and adding TypeScript

--- src/components/ImageProject/Image.jsx
+++ src/components/ImageProject/Image.jsx
@@ -1,21 +1,21 @@
-import styles from './Image.module.css'
+import styles from './Image.module.css';
 
-import {motion} from 'framer-motion'
-import { fadeIn } from '../../animations'
+import {motion} from 'framer-motion';
+import { fadeIn } from '../../animations';
 
 export const Image = (props) => {
-    const {handleShow} = props
+    const {handleShow} = props;
 
     return (
-    <motion.figure
-    key={props.key} 
-    className={styles['ImageContainer']} 
-    onClick={() => handleShow(props.src)}
-    variants={fadeIn(0.2)}
-    initial='hidden'
-    animate='show'
-     >
-        <img src={props.src} alt="Project Image"></img>  
-    </motion.figure>
-    )
+        <motion.figure
+            key={props.key} 
+            className={styles['ImageContainer']} 
+            onClick={() => handleShow(props.src)}
+            variants={fadeIn(0.2)}
+            initial='hidden'
+            animate='show'
+        >
+            <img src={props.src} alt="Project Image"></img>  
+        </motion.figure>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { SectionsProvider } from './context/sections.jsx'
-import { Details } from './pages/Details/Details.jsx'
+import { Details } from './pages/Details/Details.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SectionsProvider>
